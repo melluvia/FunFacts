a0f8b03e27ceb5c401a102d59b10d0e88fa2a088
Melissa Phillips <melissanicolephillips@gmail.com>
2016-06-17 22:29:29 -0500

    Initial Commit

---
+++ Funfacts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Funfacts
+//
+//  Created by Melissa Phillips on 6/17/16.
+//  Copyright © 2016 Melissa Phillips. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
