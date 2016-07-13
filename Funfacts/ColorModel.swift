c58f7082e3de7ee85120ac03575bc8fb57135943
Melissa Phillips <melissanicolephillips@gmail.com>
2016-07-12 21:32:34 -0500

    FunFacts

---
+++ Funfacts/ColorModel.swift
@@ -0,0 +1,9 @@
+//
+//  colormodel.swift
+//  Funfacts
+//
+//  Created by Melissa Phillips on 7/9/16.
+//  Copyright © 2016 Melissa Phillips. All rights reserved.
+//
+
+import UIKit
---
+++ Funfacts/FactModel.swift
@@ -0,0 +1,9 @@
+//
+//  FactModel.swift
+//  Funfacts
+//
+//  Created by Melissa Phillips on 6/25/16.
+//  Copyright © 2016 Melissa Phillips. All rights reserved.
+//
+
+import Foundation
